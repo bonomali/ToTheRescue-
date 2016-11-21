255a463bba4f54cdb108e1c12db4ca7d54d777ef
mattdelfante <matthew.delfante@oit.edu>
2016-11-21 14:25:21 -0800

    Renamed a controller function so my web page works.

--- WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/Controllers/ChooseProfilePageController.cs
+++ WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/Controllers/ChooseProfilePageController.cs
@@ -10,7 +10,7 @@ namespace ToTheRescueWebApplication.Controllers
     public class ChooseProfilePageController : Controller
     {
         // GET: ChooseProfilePage
-        public ActionResult Index() //was Index before
+        public ActionResult ChooseProfilePage() //was Index before
         {
             //Give the view the Model object to utilize
             ProfilesModel profiles = new ProfilesModel();
