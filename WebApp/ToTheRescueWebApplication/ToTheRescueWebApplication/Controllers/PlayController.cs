2cc069479048d266a44ee3c12a40f48c03326d20
Lake Sain-Thomason <lakesainthomason@gmail.com>
2017-01-31 14:03:26 -0800

    Gettting ready for presentation

--- WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/Controllers/PlayController.cs
+++ WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/Controllers/PlayController.cs
@@ -289,13 +289,9 @@ namespace ToTheRescueWebApplication.Controllers
                 }
             }
             model.MiniGameID = minigames[ranGame].ID;
-            //model.MiniGame = minigames[ranGame].MiniGamePath;
-            //model.MiniGame = "../../MiniGames/Alphabet_BubblePop/javascript/bubble.js"; //path to minigame (eventually on server)
-            //model.MiniGame = "../../MiniGames/ColorSortingGame/javascript/ColorSortingGame.js"; //path to minigame (eventually on server)
-            //model.MiniGame = "../../MiniGames/Number_Comparison/javascript/Number_Comparison.js"; //path to minigame (eventually on server)
-            //model.MiniGame = "../../MiniGames/Number_ComparisonEZ/javascript/Number_ComparisonEZ.js"; //path to minigame (eventually on server)
-            model.MiniGame = "../../MiniGames/Dino_SightWords/javascript/dino_sightwords.js";
+            model.MiniGame = "../../MiniGames/Shape_ColoringBook/javascript/colorbook.js";
             model.CategoryID = minigames[ranGame].MiniGameCategoryID;
+
             return View(model);
         }
     }
