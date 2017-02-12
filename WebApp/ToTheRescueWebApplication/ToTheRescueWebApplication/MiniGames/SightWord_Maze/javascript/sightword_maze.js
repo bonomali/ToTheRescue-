0184345415bd10aac198dbe9d0e47f587d102f48
stephanievetter <stephanie.vetter@oit.edu>
2017-02-11 17:34:32 -0800

    finished sight word maze game

--- WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/MiniGames/SightWord_Maze/javascript/sightword_maze.js
+++ WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/MiniGames/SightWord_Maze/javascript/sightword_maze.js
@@ -1,16 +1,36 @@
 ﻿(function () {
     var imagePath = '../../MiniGames/SightWord_Maze/images/';
-    var audioPath = '../../MiniGames/SightWord_Maze/sounds/';
-    var playing = true;
+    var soundPath = '../../MiniGames/SightWord_Maze/sounds/';
+    var counter = -1;   //keep track of which word in maze the pup is on
+    var score = 0;
+    var audioInstructions = new Audio();
+    audioInstructions.src = soundPath + "audio_instructions.mp3";
+    var endOfGame = new Audio();
+    var correctWord = new Audio();
+    var wrongWay = new Audio();
+    wrongWay.src = soundPath + "bark.mp3";
 
     words = ["at", "can", "in", "big", "me", "not", "on", "see", "the", "up"];
-   /* wordSounds = [soundPath + "at_recording.mp3", soundPath + "can_recording.mp3", soundPath + "in_recording.mp3",
+    wordSounds = [soundPath + "at_recording.mp3", soundPath + "can_recording.mp3", soundPath + "in_recording.mp3",
         soundPath + "big_recording.mp3", soundPath + "me_recording.mp3", soundPath + "not_recording.mp3",
         soundPath + "on_recording.mp3", soundPath + "see_recording.mp3", soundPath + "the_recording.mp3",
         soundPath + "up_recording.mp3"];
-        */
+      
+
+    //randomly select indecies for target word and for three other words for maze
     var targetIndex = Math.floor((Math.random() * words.length)); //random number for array index
     var targetWord = words[targetIndex];    //target word for maze
+    correctWord.src = wordSounds[targetIndex];  //sound of target word
+
+    var index1 = Math.floor((Math.random() * words.length)); //random number for array index
+    while (index1 == targetIndex)
+        index1 = Math.floor((Math.random() * words.length)); //random number for array index
+    var index2 = Math.floor((Math.random() * words.length)); //random number for array index
+    while (index2 == targetIndex || index2 == index1)
+        index2 = Math.floor((Math.random() * words.length)); //random number for array index
+    var index3 = Math.floor((Math.random() * words.length)); //random number for array index
+    while (index3 == targetIndex || index3 == index2 || index3 == index1)
+        index3 = Math.floor((Math.random() * words.length)); //random number for array index
 
     //set correct margin for div and create paragraph elements
     var blocksDiv = document.getElementById('BlocksGame');
@@ -21,6 +41,7 @@
     blocksDiv.style.backgroundRepeat = "no-repeat";
     blocksDiv.style.backgroundSize = "cover";
 
+    //create dog image element
     var dog = document.createElement('img');
     dog.setAttribute('id', 'dog');
     dog.src = imagePath + "dog.png";
@@ -28,6 +49,7 @@
     dog.style.position = "absolute";
     dog.style.width = '6%';
 
+    //create target word elements
     var text1 = document.createElement('p');
     text1.setAttribute('class', 'word');
     blocksDiv.appendChild(text1);
@@ -118,6 +140,124 @@
     blocksDiv.appendChild(text18);
     text18.innerHTML = targetWord;
 
+
+    //create other words for maze
+    var text19 = document.createElement('p');
+    text19.setAttribute('class', 'word');
+    blocksDiv.appendChild(text19);
+    text19.innerHTML = words[index1];
+
+    var text20 = document.createElement('p');
+    text20.setAttribute('class', 'word');
+    blocksDiv.appendChild(text20);
+    text20.innerHTML = words[index2];
+
+    var text21 = document.createElement('p');
+    text21.setAttribute('class', 'word');
+    blocksDiv.appendChild(text21);
+    text21.innerHTML = words[index3];
+
+    var text22 = document.createElement('p');
+    text22.setAttribute('class', 'word');
+    blocksDiv.appendChild(text22);
+    text22.innerHTML = words[index1];
+
+    var text23 = document.createElement('p');
+    text23.setAttribute('class', 'word');
+    blocksDiv.appendChild(text23);
+    text23.innerHTML = words[index2];
+
+    var text24 = document.createElement('p');
+    text24.setAttribute('class', 'word');
+    blocksDiv.appendChild(text24);
+    text24.innerHTML = words[index3];
+
+    var text25 = document.createElement('p');
+    text25.setAttribute('class', 'word');
+    blocksDiv.appendChild(text25);
+    text25.innerHTML = words[index1];
+
+    var text26 = document.createElement('p');
+    text26.setAttribute('class', 'word');
+    blocksDiv.appendChild(text26);
+    text26.innerHTML = words[index2];
+
+    var text27 = document.createElement('p');
+    text27.setAttribute('class', 'word');
+    blocksDiv.appendChild(text27);
+    text27.innerHTML = words[index3];
+
+    var text28 = document.createElement('p');
+    text28.setAttribute('class', 'word');
+    blocksDiv.appendChild(text28);
+    text28.innerHTML = words[index1];
+
+    var text29 = document.createElement('p');
+    text29.setAttribute('class', 'word');
+    blocksDiv.appendChild(text29);
+    text29.innerHTML = words[index2];
+
+    var text30 = document.createElement('p');
+    text30.setAttribute('class', 'word');
+    blocksDiv.appendChild(text30);
+    text30.innerHTML = words[index3];
+
+    var text31 = document.createElement('p');
+    text31.setAttribute('class', 'word');
+    blocksDiv.appendChild(text31);
+    text31.innerHTML = words[index1];
+
+    var text32 = document.createElement('p');
+    text32.setAttribute('class', 'word');
+    blocksDiv.appendChild(text32);
+    text32.innerHTML = words[index2];
+
+    var text33 = document.createElement('p');
+    text33.setAttribute('class', 'word');
+    blocksDiv.appendChild(text33);
+    text33.innerHTML = words[index3];
+
+    var text34 = document.createElement('p');
+    text34.setAttribute('class', 'word');
+    blocksDiv.appendChild(text34);
+    text34.innerHTML = words[index1];
+
+    var text35 = document.createElement('p');
+    text35.setAttribute('class', 'word');
+    blocksDiv.appendChild(text35);
+    text35.innerHTML = words[index2];
+
+    var text36 = document.createElement('p');
+    text36.setAttribute('class', 'word');
+    blocksDiv.appendChild(text36);
+    text36.innerHTML = words[index3];
+
+    var text37 = document.createElement('p');
+    text37.setAttribute('class', 'word');
+    blocksDiv.appendChild(text37);
+    text37.innerHTML = words[index1];
+
+    var text38 = document.createElement('p');
+    text38.setAttribute('class', 'word');
+    blocksDiv.appendChild(text38);
+    text38.innerHTML = words[index2];
+
+    var text39 = document.createElement('p');
+    text39.setAttribute('class', 'word');
+    blocksDiv.appendChild(text39);
+    text39.innerHTML = words[index3];
+
+    var text40 = document.createElement('p');
+    text40.setAttribute('class', 'word');
+    blocksDiv.appendChild(text40);
+    text40.innerHTML = words[index1];
+
+    var text41 = document.createElement('p');
+    text41.setAttribute('class', 'word');
+    blocksDiv.appendChild(text41);
+    text41.innerHTML = words[index2];
+
+    //set style for words
     var wordsList = document.getElementsByClassName('word');
     for (var i = 0; i < wordsList.length; i++)
     {
@@ -149,6 +289,8 @@
             }
         }
     }
+
+    //set position of target words
     text1.style.left = '65%';
     text1.style.top = '1%';
 
@@ -203,85 +345,274 @@
     text18.style.left = '20%';
     text18.style.top = '87%';
 
+    text19.style.left = '62%';
+    text19.style.top = '15%';
+
+    text20.style.left = '62%';
+    text20.style.top = '32%';
+
+    text21.style.left = '77%';
+    text21.style.top = '15%';
+
+    text22.style.left = '77%';
+    text22.style.top = '32%';
+
+    text23.style.left = '77%';
+    text23.style.top = '80%';
+
+    text24.style.left = '85%';
+    text24.style.top = '87%';
+
+    text25.style.left = '92%';
+    text25.style.top = '80%';
+
+    text26.style.left = '92%';
+    text26.style.top = '64%';
+
+    text27.style.left = '92%';
+    text27.style.top = '50%';
+
+    text28.style.left = '93%';
+    text28.style.top = '32%';
+
+    text29.style.left = '93%';
+    text29.style.top = '15%';
+
+    text30.style.left = '35%';
+    text30.style.top = '28%';
+
+    text31.style.left = '20%';
+    text31.style.top = '28%';
+
+    text32.style.left = '5%';
+    text32.style.top = '28%';
+
+    text33.style.left = '35%';
+    text33.style.top = '47%';
+
+    text34.style.left = '35%';
+    text34.style.top = '65%';
+
+    text35.style.left = '28%';
+    text35.style.top = '74%';
+
+    text36.style.left = '15%';
+    text36.style.top = '74%';
+
+    text37.style.left = '4%';
+    text37.style.top = '74%';
+
+    text38.style.left = '4%';
+    text38.style.top = '58%';
+
+    text39.style.left = '4%';
+    text39.style.top = '42%';
+
+    text40.style.left = '22%';
+    text40.style.top = '42%';
+
+    text41.style.left = '22%';
+    text41.style.top = '58%';
+
     dog.style.left = '5%';
     dog.style.top = '90%';
     dog.style.zIndex = '2';
 
-    var correctWord = false;
-
-
+    //click events
     $(text18).click(function () {
-        dog.style.left = '20%';
-        dog.style.top = '87%';
+        if (counter == -1 || counter == 17) {
+            dog.style.left = '20%';
+            dog.style.top = '87%';
+            counter = 18;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text17).click(function () {
-        dog.style.top = '87%';
-        dog.style.left = '35%';
+        if (counter == 18 || counter == 16) {
+            dog.style.top = '87%';
+            dog.style.left = '35%';
+            counter = 17;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text16).click(function () {
-        dog.style.top = '87%';
-        dog.style.left = '50%';
+        if (counter == 17 || counter == 15) {
+            dog.style.top = '87%';
+            dog.style.left = '50%';
+            counter = 16;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text15).click(function () {
-        dog.style.left = '62%';
-        dog.style.top = '82%';
+        if (counter == 14 || counter == 16) {
+            dog.style.left = '62%';
+            dog.style.top = '82%';
+            counter = 15;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text14).click(function () {
-        dog.style.left = '47%';
-        dog.style.top = '70%';
+        if (counter == 13 || counter == 15) {
+            dog.style.left = '47%';
+            dog.style.top = '70%';
+            counter = 14;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text13).click(function () {
-        dog.style.left = '62%';
-        dog.style.top = '64%';
+        if (counter == 12 || counter == 14) {
+            dog.style.left = '62%';
+            dog.style.top = '64%';
+            counter = 13;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text12).click(function () {
-        dog.style.left = '77%';
-        dog.style.top = '64%';
+        if (counter == 11 || counter == 13) {
+            dog.style.left = '77%';
+            dog.style.top = '64%';
+            counter = 12;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text11).click(function () {
-        dog.src = imagePath + "mirrored_dog.png";
-        dog.style.left = '77%';
-        dog.style.top = '50%';
+        if (counter == 10 || counter == 12) {
+            dog.src = imagePath + "mirrored_dog.png";
+            dog.style.left = '77%';
+            dog.style.top = '50%';
+            counter = 11;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text10).click(function () {
-        dog.style.left = '62%';
-        dog.style.top = '50%';
+        if (counter == 9 || counter == 11) {
+            dog.style.left = '62%';
+            dog.style.top = '50%';
+            counter = 10;
+        }
     });
     $(text9).click(function () {
-        dog.style.left = '47%';
-        dog.style.top = '50%';
+        if (counter == 8 || counter == 10) {
+            dog.style.left = '47%';
+            dog.style.top = '50%';
+            counter = 9;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text8).click(function () {
-        dog.style.left = '47%';
-        dog.style.top = '33%';
+        if (counter == 7 || counter == 9) {
+            dog.style.left = '47%';
+            dog.style.top = '33%';
+            counter = 8;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text7).click(function () {
-        dog.style.left = '47%';
-        dog.style.top = '15%';
+        if (counter == 6 || counter == 8) {
+            dog.style.left = '47%';
+            dog.style.top = '15%';
+            counter = 7;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text6).click(function () {
-        dog.style.left = '25%';
-        dog.style.top = '15%';
+        if (counter == 5 || counter == 7) {
+            dog.style.left = '25%';
+            dog.style.top = '15%';
+            counter = 6;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text5).click(function () {
-        dog.style.left = '5%';
-        dog.style.top = '15%';
+        if (counter == 4 || counter == 6) {
+            dog.style.left = '5%';
+            dog.style.top = '15%';
+            counter = 5;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text4).click(function () {
-        dog.src = imagePath + "dog.png";
-        dog.style.left = '5%';
-        dog.style.top = '1%';
+        if (counter == 3 || counter == 5) {
+            dog.src = imagePath + "dog.png";
+            dog.style.left = '5%';
+            dog.style.top = '1%';
+            counter = 4;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text3).click(function () {
-        dog.style.left = '25%';
-        dog.style.top = '1%';
+        if (counter == 2 || counter == 4) {
+            dog.style.left = '25%';
+            dog.style.top = '1%';
+            counter = 3;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text2).click(function () {
-        dog.style.left = '45%';
-        dog.style.top = '1%';
+        if (counter == 1 || counter == 3) {
+            dog.style.left = '45%';
+            dog.style.top = '1%';
+            counter = 2;
+            correctWord.play();
+            score = score + 5;
+        }
     });
     $(text1).click(function () {
-        dog.style.left = '80%';
-        dog.style.top = '3%';
+        if (counter == 2) {
+            dog.style.left = '80%';
+            dog.style.top = '3%';
+            endOfGame.src = soundPath + "puppy_barking.mp3";
+            endOfGame.play();
+        }
+    });
+    $(text23).click(function () {
+        if (counter == 12) {
+            score = score - 10;
+            wrongWay.play();
+        }
+    });
+    $(text22).click(function () {
+        if (counter == 11) {
+            score = score - 10;
+            wrongWay.play();
+        }
+    });
+    $(text30).click(function () {
+        if (counter == 8 || counter == 7) {
+            score = score - 10;
+            wrongWay.play();
+        }
+    });
+
+    endOfGame.addEventListener("ended", function () {
+        var finalScore = -5;
+        document.getElementById('BlocksGame').style.top = '68%';
+        document.getElementById('gameOver').style.top = '20%';
+        if (score > 80) { finalScore = 5; }
+        else if (score > 60) { finalScore = 3; }
+        else if (score > 40) { finalScore = 0; }
+
+        document.getElementById('score').value = finalScore; //save score in html element
+        EndofGame(); //function displays good job message and returns to map
+    });
+
+    audioInstructions.play();
+    audioInstructions.addEventListener("ended", function() {
+        correctWord.play();
     });
 }());
