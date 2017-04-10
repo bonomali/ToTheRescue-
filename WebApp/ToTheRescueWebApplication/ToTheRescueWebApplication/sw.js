d94035a30a2aa9020dd7a255591a6f4a0cf94534
RobertMooers <robert.mooers@oit.edu>
2017-04-10 00:19:42 -0700

    Fixed App Manifest for Being a Progressive Web App?

---
+++ WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/sw.js
@@ -0,0 +1,17 @@
+﻿console.log('I am a Service Worker!');
+
+self.addEventListener('install', event => {
+    // Bypass the waiting lifecycle stage,
+    // just in case there's an older version of this SW registration.
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+    // Take control of all pages under this SW's scope immediately,
+    // instead of waiting for reload/navigation.
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    // In a real app, you'd use a more sophisticated URL check.
+});
