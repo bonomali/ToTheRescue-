e9d1834c7260e6717d8fc469c854cd9cc45b0b8c
Lake Sain-Thomason <lakesainthomason@gmail.com>
2017-05-01 08:17:42 -0700

    Quick fix to Number_Find
    Eliminated the possibility of being able to have the same random numbers
    in the numsToFind array

--- WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/MiniGames/Number_Find/javascript/Number_Find.js
+++ WebApp/ToTheRescueWebApplication/ToTheRescueWebApplication/MiniGames/Number_Find/javascript/Number_Find.js
@@ -51,7 +51,10 @@ var initializeVars = function () {
     //set the nums to be found for the game
     for (var i = 0; i < maxNumber; i++)
     {
-        numsToFind[i] = Math.floor(Math.random() * 100) + 1;
+        var rand = Math.floor(Math.random() * 100) + 1;
+        for (var k = 0; k < i; k++)
+            if (numsToFind[k] != numsToFind[i])
+                numsToFind[i] = i;
     }
     //set the cat expression pictures
     catPics = [imgPath + "default.png", imgPath + "disapointed.png"
